30a7775b795709022e08a0b05de8809f018e4b84
ebaadwaseem <ebaadwaseem@gmail.com>
2023-03-09 09:28:08 -0600

    Fixed Ebaad_RPG_Menu.py

--- Ebaad_RPG_Menu.py
+++ Ebaad_RPG_Menu.py
@@ -1,12 +1,19 @@
-actions = ["1. Attack", "2. Defend", "3. Look Around", "4. Quit"]
+# Ebaad Waseem
+# CS 30 Period 1
+# March 9, 2023,
+# This is a very simple RPG Game Menu
 
+# Defined actions that the user can take
+actions = ["1. Attack", "2. Defend", "3. Look Around", "4. Quit"]
+# Printing all items inside the array
 for items in actions:
     print(items)
+# Runs indefinitely until it breaks
 while True:
-
-    choice = input("Choose a number between 1-4: ")
-
-    if not choice.isnumeric():
+    # Handles user inputs, a number between 1 and 4
+    choice = input("Choose a number between 1 - 4: ")
+    # Checks the user input, handles if not a number and breaks when needed
+    if not choice.isnumeric() :
         print("Incorrect input, chose a number between 1 and 4")
     else:
         if int(choice) == 1:
@@ -18,3 +25,5 @@ while True:
         elif int(choice) == 4:
             print("Exiting game, thank you for playing!")
             break
+        else:
+            print("Incorrect input, chose a number between 1 and 4")
